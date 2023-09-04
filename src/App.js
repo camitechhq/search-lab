69f00c6f1244ac17a3b3886f19e5ab274386a6a9
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-09-04 23:20:13 +0530

    setup: initial react dev setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <div>
+          Search Lab React JS
+        </div>
+      </center>
+    </div>
+  );
+}
+
+export default App;
